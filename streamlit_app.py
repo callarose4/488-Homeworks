1549e2467480328447a019aa06dffb611c6154c4
callarose4 <crkolar@syr.edu>
2026-01-22 02:27:58 +0000

    Changed model to gpt 4.1

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ if uploaded_file and question:
     ]
 
     stream = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4.1-turbo",
         messages=messages,
         stream=True,
     )
